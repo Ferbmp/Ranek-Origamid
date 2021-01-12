cf51cf0dbe33b537c9faa5880cee8c066d3612c3
FernandoMonteiro <fernando.monteiro@dt3.com.br>
2021-01-12 09:48:32 -0300

    Completo

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -24,15 +24,18 @@ function Produto() {
     }
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <div className="loading"></div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
     <section className={styles.produto + ' animeLeft'}>
       <Head title={`Ranek | ${produto.nome}`} />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R${produto.preco}</span>
